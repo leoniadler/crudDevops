62b3f2c7f0fdc11817fdadefd8b7fcd9542306ea
Leoni Adler <leoniadler@MacBook-Air-de-Leoni.local>
2023-04-08 02:47:14 -0300

    commit 2

---
+++ Terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "1.4.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.16"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
